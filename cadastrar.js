459ab789b3b9efb25703e42c2d2e96c515486bb7
Luana dos Anjos Siqueira <luanasiqueira5535@gmail.com>
2024-07-23 16:46:12 -0300

    Update cadastrar.js
    Menu

--- cadastrar.js
+++ cadastrar.js
@@ -1,5 +1,48 @@
-const prompt = require('prompt-sync')()
+const prompt = require ('prompt-sync') ()
 
+let sessoes = []
+
+exibirMenu()
+function exibirMenu() {
+    console.log(`
+      🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿
+    🍿 1- Adicionar sessão;                 🍿
+    🍿 2- Listar sessões;                   🍿
+    🍿 3- Atualizar uma sessão existente;   🍿
+    🍿 4- Cancelar uma sessão;              🍿
+    🍿 5- Sair                              🍿
+      🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿🍿
+    `)
+
+    function cinema(sessoes) {
+        switch(opcão) {
+            case '1':
+                return adicionar()
+                break;
+
+            case '2':
+                return listar()
+                break;
+
+            case '3':
+                return atualizar() 
+                break;
+
+            case '4':
+                return cancelar()
+                break;
+
+            case '5':
+                rl.close()
+                break;
+
+            default:
+                console.log('Opção inválida.')
+                exibirMenu() 
+                break;
+        }
+    }
+}
 function cadastrar(){
     if(cinema.length == 0){
         console.log('nao ha nenhuma sessao cadastrada ainda')
