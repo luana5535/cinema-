b80822ed4db46c3a5171f881d1de85c455e5b678
Wanderle1 <wanderlei.mora3s@gmail.com>
2024-07-23 16:25:20 -0300

    listar

---
+++ listar.js
@@ -0,0 +1,14 @@
+function listar() {
+    if (cinema.length == 0) {
+      console.log("Não há nenhum Filme Agendado");
+    } else {
+      console.log("Lista dos Filmes Agendados: ");
+      carros.forEach((cinema, index) => {
+        console.log(
+          `${index + 1}. Filme: ${cinema.nomeFilme}, Data: ${cinema.data}, hora: ${cinema.hora} Sala: ${cinema.sala}`
+        );
+      });
+    }
+  }
+
+  module.exports = {listar}
