081b5ec8bd3a739826a01736d8f8d3f09f57c6f4
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-07-24 14:34:49 -0300

    Update listar.js

--- listar.js
+++ listar.js
@@ -2,7 +2,7 @@ function listar(cinema) {
     if (cinema.length == 0) {
       console.log("Não há nenhum Filme Agendado");
     } else {
-      console.log("Lista dos Filmes Agendados: ");
+      console.log("Lista dos Filmes Agendados 🍿🍿🍿 ");
       cinema.forEach((cinema, index) => {
         console.log(
           `${index + 1}. Filme: ${cinema.nomeFilme}, Data: ${cinema.data}, hora: ${cinema.hora} Sala: ${cinema.sala}`
