a9025eeb6eb7face889f30192f3f36261a6c5958
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-07-24 14:32:10 -0300

    Update cadastrar.js

--- cadastrar.js
+++ cadastrar.js
@@ -2,10 +2,10 @@ const prompt = require('prompt-sync')()
 
 function cadastrar(cinema,prompt,callback){
 
-        let nomeFilme = prompt('digite o nome do filme: ')
-        let data = prompt('digite a data do filme: ')
-        let hora = prompt('digite a hora do filme: ')
-        let sala = prompt('digite a sala da sessão: ')
+        let nomeFilme = prompt('digite o nome do filme 👉 ')
+        let data = prompt('digite a data do filme 👉 ')
+        let hora = prompt('digite a hora do filme 👉 ')
+        let sala = prompt('digite a sala da sessão 👉 ')
         cinema.push({nomeFilme: nomeFilme, data: data, hora: hora, sala: sala})
         console.log('sessão cadastrada com sucesso!')
         callback()
