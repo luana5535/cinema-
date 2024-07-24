4bcdd39f8c20d4e31aa0d018ce2e45628a94f2e6
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-07-24 15:10:15 -0300

    Update cancelar.js

--- cancelar.js
+++ cancelar.js
@@ -5,7 +5,7 @@ function cancelar(prompt,cinema,callback) {
     let resp = prompt('Digite o numero do Filme que quer Cancelar 👉 ') 
       if (resp > 0 && resp <= cinema.length) {
         cinema.splice(resp - 1, 1);
-        console.log("Filme cancelado com sucesso!");
+        console.log("Filme cancelado com sucesso!📽️");
         callback();
       } else {
         console.log("numero invalido,tente novamente");
