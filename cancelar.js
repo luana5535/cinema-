ea5f091fe88bd40ade137de75ea9ebc7cec50d18
MiguelDaBoa <braidinho12@gmail.com>
2024-07-24 14:23:10 -0300

    Update cancelar.js

--- cancelar.js
+++ cancelar.js
@@ -2,7 +2,7 @@ const {listar} = require('./listar')
 
 function cancelar(prompt,cinema,callback) {
     listar(cinema)
-    let resp = prompt('Digite o numero do Filme que quer Cancelar') 
+    let resp = prompt('Digite o numero do Filme que quer Cancelar: ') 
       if (resp > 0 && resp <= cinema.length) {
         cinema.splice(resp - 1, 1);
         console.log("Filme cancelado com sucesso!");
