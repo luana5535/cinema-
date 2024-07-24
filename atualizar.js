4581db88d8e0cd08d781ad64e71a157ed066ca9a
Wanderlei de Moraes <wanderlei.mora3s@gmail.com>
2024-07-24 14:32:56 -0300

    Update atualizar.js

--- atualizar.js
+++ atualizar.js
@@ -5,12 +5,12 @@ if(cinema.length == 0){
     callback()
 }else{
     listar(cinema)
-    let numero = prompt('digite o numero do filme que voce quer editar: ')
+    let numero = prompt('digite o numero do filme que voce quer editar 👉 ')
     if(numero > 0 && numero <= cinema.length){
-    let novoFilme = prompt('digite o novo nome do filme/atualize o nome do filme: ')
-    let novaData = prompt('digite a nova data: ')
-    let novoHorario = prompt('digite o novo/atualizado horario: ')
-    let novaSala = prompt('digite a nova sala: ')
+    let novoFilme = prompt('digite o novo nome do filme/atualize o nome do filme 👉 ')
+    let novaData = prompt('digite a nova data 👉 ')
+    let novoHorario = prompt('digite o novo/atualizado horario 👉 ')
+    let novaSala = prompt('digite a nova sala 👉 ')
     cinema[numero -1] = {
         nomeFilme: novoFilme,
         data: novaData,
